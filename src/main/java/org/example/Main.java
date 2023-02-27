28fc923c70cddcf7821022c5571189ad3e24fbd3
Alexandr <jwchaganav@gmail.com>
2023-02-27 11:11:46 +0400

    вывел результат

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,33 +6,39 @@ import java.util.HashSet;
 public class Main {
     public static void main(String[] args) {
         HashMap<String,String> hashMap = new HashMap<>();
-        hashMap.put("Первый","Вася");
-        hashMap.put("Пятый","Кеша");
-        hashMap.put("Третий","Петя");
-        hashMap.putIfAbsent("Четвертый","Миша");
+        hashMap.put("1","Вася");
+        hashMap.put("5","Кеша");
+        hashMap.put("3","Петя");
+        hashMap.putIfAbsent("4","Миша");
+        hashMap.put("9","Коля");
+        hashMap.put("7","Jon");
+        hashMap.put("8","Micle");
+        // переберем и выведем в консоль
         hashMap.forEach((k,v)-> System.out.println(k+": "+v));
         System.out.println("-".repeat(15));
-        for(String v: hashMap.values()){
-            System.out.println(v+" ! !");
+        // добавим символ !
+        for(String k: hashMap.keySet()){
+            hashMap.compute(k,(key,v)-> v + " !");
+            System.out.println(hashMap.get(k));
         }
         System.out.println("-".repeat(15));
-        for(String k: hashMap.keySet()){
-            System.out.println(k+": "+hashMap.get(k));
-        }
+
         for(String key: hashMap.keySet()) {
             hashMap.compute(key, (k, v) -> "<" + v + ">");
             System.out.println(hashMap.get(key));
         }
         System.out.println(hashMap);
         System.out.println("-".repeat(15));
+
         HashMap<String, String> hashMap2 = new HashMap<>();
-        hashMap2.put("Первый","Оля");
-        hashMap2.put("Четвертый","Маша");
-        hashMap2.put("Пятый","Марина");
-        hashMap2.put("Шестой","Елена");
+        hashMap2.put("1","Оля");
+        hashMap2.put("4","Маша");
+        hashMap2.put("5","Марина");
+        hashMap2.put("6","Елена");
+        hashMap2.put("8","Marry");
         for(String k: hashMap2.keySet()){
             //hashMap2.merge(k,hashMap.getOrDefault(k,"No"),String::concat);
-            hashMap2.merge(k,hashMap.getOrDefault(k,"Одна осталась (:"),(key,val)-> key + " + "+val);
+            hashMap2.merge(k,hashMap.getOrDefault(k,"??"),(key,val)-> key + " + "+val);
             System.out.println(hashMap2.get(k).replace("<","").replace(">",""));
         }
     }
